972bf3baed92590f0c04608f8be5f2bf781d6586
Michael Stafford <michaelstafford808@gmail.com>
2022-02-09 11:09:54 -1000

    commit tree cf4d627a7aba5811339b8ec93023e284867e4242

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
 /* Your Code Here */
 
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+}
+
+const createTimeInEvent = function (dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    // const arrFromDate = dateStamp.split(" ")
+    // const date = arrFromDate[0]
+    // const hour = arrFromDate[1]
+
+    const inEvent = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date,
+    }
+
+    this.timeInEvents.push(inEvent);
+
+    return this;
+}
+
+
+const createTimeOutEvent = function (dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+   
+
+    const outEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date,
+    }
+
+    this.timeOutEvents.push(outEvent);
+
+    return this;
+}
+
+
+const hoursWorkedOnDate = function (targetDate){
+    // console.log("IN EVENT", inEvent);
+    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+    const outEvent = this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
+    return (outEvent.hour - inEvent.hour) / 100;
+}
+
+
+const wagesEarnedOnDate = function(targetDate){
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
+
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +85,18 @@ const allWagesFor = function () {
     return payable
 }
 
+
+const findEmployeeByFirstName = function(srcArray, firstName){
+    console.log("ARRAY", srcArray)
+    console.log("FIRST NAME", firstName)
+    return srcArray.find(rec => rec.firstName === firstName)
+    
+}
+
+
+const calculatePayroll = function (recsArray){
+    return recsArray.reduce((total, rec) => {
+        return total + allWagesFor.call(rec)
+    }, 0)
+     
+}
